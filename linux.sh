f51fbabc2f8653e8dce697b935c05f10582db995
Datatech <74945888+roshan648@users.noreply.github.com>
2021-08-26 15:49:33 +0530

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,11 @@
+cd templates
+sudo apt update 
+sudo apt install php
+sudo apt install zip wget -y
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+chmod +x ngrok
+echo "Eg:./ngrok authtoken asdfff"
+echo "Go to https://ngrok.com/ and login to get token."
+read -p "Enter the token :" text
+$text
