37dd4ede9c1e08a5a575979feb9fcdf44ef975f1
Datatech <74945888+roshan648@users.noreply.github.com>
2021-08-26 15:50:58 +0530

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -6,5 +6,6 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
 chmod +x ngrok
 echo "Eg:./ngrok authtoken asdfff"
+echo "Go to https://ngrok.com/ and login to get token."
 read -p "Enter the tocken :" text
 $text
