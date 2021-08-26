c0de4641f9b9fa73ef6b43993a50a2b5cb8db93c
Datatech <74945888+roshan648@users.noreply.github.com>
2021-08-26 12:56:49 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+import os 
+from pathlib import Path
+text = 'FF-punch'
+os.system("cfonts "+text+" -f block -c bright_red ")
+print('1 - Choose gift + Package gift + Lucky spin.')
+print('2 - Garena gift centre.')
+print('3 - Magic event reward.')
+print('4 - Redemption Code.')
+print('5 - Diamond Buy.')
+print('6 - Exit.')
